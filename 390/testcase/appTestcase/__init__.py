3cbe315c900150eda55c3d8c39530d74fbc030db
xinxi <xinxi@luojilab.com>
2020-08-20 23:35:34 +0800

    add

--- 390/testcase/appTestcase/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-'''=================================================
-@Project -> File   ：autoui_base -> __init__.py
-@IDE    ：PyCharm
-@Author ：GAI
-@Date   ：2020/7/18 6:41 下午
-@Desc   ：
-=================================================='''
