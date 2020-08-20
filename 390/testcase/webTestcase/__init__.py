686fe050c561522a5888c88534a0bc214c089b4b
xinxi <xinxi@luojilab.com>
2020-08-20 16:34:42 +0800

    add

---
+++ 390/testcase/webTestcase/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：autoui_base -> __init__.py
+@IDE    ：PyCharm
+@Author ：GAI
+@Date   ：2020/7/18 4:00 下午
+@Desc   ：
+=================================================='''
