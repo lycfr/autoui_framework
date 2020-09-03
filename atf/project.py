ccd557d05c3208498ab0ee9298341afe6e4545a3
xinxi <xinxi@luojilab.com>
2020-09-03 17:13:53 +0800

    update

--- atf/project.py
+++ atf/project.py
@@ -78,8 +78,6 @@ class Project(object):
             else:
                 Var.retry_count = 0
 
-            log_info("******************* 最大重试用例次数 {} *******************".format(Var.retry_count))
-
         if "appdriver" in self.__config.keys():
             log_info("******************* appdriver 初始化 *******************")
             AppDriverBase.init()
